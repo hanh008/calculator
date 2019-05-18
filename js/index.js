87eb88f40f75f8456b7174fbe72c24a1529856b8
Hanh <hanh@Hanhs-MacBook-Air.local>
2019-05-18 19:05:29 +1200

    add pseudocode

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// create Array
+
+// create string
+
+// create display variable
+
+// initialise listen 
+
+// create listen function using event listener
+
+// create get button value function using if and else statements
+
+// create number function using if and else statements
+
+// create all clear function
+
+// create store number function us if and else statements
+
+// create calculate function using if and else statements and for loop
